854d401092a153764ea53d60cc7d09949b8eb5c6
Frank Leromain <fleromain@gmail.com>
2022-05-09 11:33:28 +0200

    feat: typings new methods

--- lib/typings/react-native-crypto.d.ts
+++ lib/typings/react-native-crypto.d.ts
@@ -10,6 +10,7 @@ declare module '@trackforce/react-native-crypto' {
   namespace Aes {
     export function encrypt(text: string, key: string, iv: string): Promise<string>;
     export function decrypt(ciphertext: string, key: string, iv: string): Promise<string>;
+    export function decryptTo64(ciphertext: string, key: string, iv: string): Promise<string>;
   }
 
   namespace Sha {
@@ -24,6 +25,7 @@ declare module '@trackforce/react-native-crypto' {
 
   namespace Pbkdf2 {
     export function hash(password: string, saltBase64: string, iterations: number, keyLen: number, hash: 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
+    export function hash64(passwordBase64: string, saltBase64: string, iterations: number, keyLen: number, hash: 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
   }
 
   namespace Rsa {
@@ -32,6 +34,7 @@ declare module '@trackforce/react-native-crypto' {
     export function encrypt64(data64: string, key: string): Promise<string>;
     export function decrypt(data: string, key: string): Promise<string>;
     export function decrypt64(data64: string, key: string): Promise<string>;
+    export function decrypt64OAEP(data64: string, key: string): Promise<string>;
     export function sign(data: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
     export function sign64(data64: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
     export function verify(signature: string, data: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<boolean>;
